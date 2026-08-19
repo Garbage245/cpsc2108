6042ea069a804e1b616b7674e6b7113171ab77bd
Garbage245 <mbsagoii@gmail.com>
2026-08-19 04:11:46 -0400

    Outline for blackjack game

---
+++ blackjack.java
@@ -0,0 +1,17 @@
+public class blackjack{
+    //Things to do
+    // 1) make a deck object that contains 52 cards (array)
+    // 2) some type of user input (ask user if they want to hit, stand, or fold)
+    // 3) add chips to dealer and player so there's a way to win. (maybe give player an option to set amount)
+    // 4) prompt player to put set an amount of chips in pot. dealer copies player value
+    // 5) code ace to function as a 1 or 11. 11 when hand is <22 and 1 when total is >22
+    // 6) code dealer to draw cards after player stands. dealer stops drawing when hand >17 or dealer busts
+    // 7) make a display to show chip counter, player hand, dealer hand, and total pot
+    // 8) when player or dealer wins add pot to winner's chip counter and remove amount from loser's
+    // 9) game over/won screen
+    // 10) code suits if i feel like it
+
+    public static void main(String[] args){
+
+    }
+}
