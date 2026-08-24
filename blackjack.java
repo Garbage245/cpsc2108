596c6f1d7f095ffcf2a4198c24318e7e64d491fe
Garbage245 <mbsagoii@gmail.com>
2026-08-24 04:35:22 -0400

    Update blackjack.java
    Coded Deck object. Currently figuring out how to make a shuffle mechanic. As of now, it takes forever to shuffle.

--- blackjack.java
+++ blackjack.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class blackjack{
     //Things to do
     // 1) make a deck object that contains 52 cards (array)
@@ -10,8 +11,76 @@ public class blackjack{
     // 8) when player or dealer wins add pot to winner's chip counter and remove amount from loser's
     // 9) game over/won screen
     // 10) code suits if i feel like it
+    
+    // Standard deck of playing cards using a String array of card values
+    public class Deck{
+        private static String[] reset = {"A", "A", "A", "A", "2", "2", "2", "2", "3", "3", "3", "3", "4", "4", "4", "4",
+                                    "5", "5", "5", "5", "6", "6", "6", "6", "7", "7", "7", "7", "8", "8", "8", "8",
+                                    "9", "9", "9", "9", "10", "10", "10", "10", "J", "J", "J", "J", "Q", "Q", "Q", "Q",
+                                    "K", "K", "K", "K"};
+        private static String[] cards = new String[52];
+        
+        public static int draw(){
+            int number = (int)Math.random()*(52);
+            if (cards[number].equals("J") || cards[number].equals("Q") || cards[number].equals("K")){
+                cards[number] = null;
+                return 10;
+            }
+            else if (cards[number].equals("A")){
+                if (PlayerStats.hand + 11 > 21 || DealerStats.hand > 21){
+                    cards[number] = null;
+                    return 1;
+                }
+                else{
+                    cards[number] = null;
+                    return 11;
+                }
+            }
+            else{
+                int cardValue =  Integer.parseInt(cards[number]);
+                cards[number] = null;
+                return cardValue;
+            }
+        }
+
+        public static void shuffle(){
+            cards = reset;
+            String[] shuffledDeck = new String[52];
+            for (int i = 0; i < cards.length; i++){
+                int number = (int)Math.random()*(52);
+                if (shuffledDeck[number] == null){
+                    shuffledDeck[number] = cards[i];
+                }
+                else{
+                    i--;
+                }
+            }
+        }
+    }
+
+    //Stats for player and dealer 
+    public class PlayerStats{
+        private static int hand;
+        private static int chips;
+    }
+
+    public class DealerStats{
+        private static int hand;
+        private static int chips;
+    }
+
+    // Uses Player and Dealer stat objects to create a score display
+    public class ScoreBoard{
+        public ScoreBoard(PlayerStats player, DealerStats dealer){
+
+        }
+    }
+
+    public void action(String input){}
 
     public static void main(String[] args){
-
+        Scanner helpy = new Scanner(System.in);
+        // Deck.shuffle();
+        System.out.println("Shuffle complete");
     }
 }
